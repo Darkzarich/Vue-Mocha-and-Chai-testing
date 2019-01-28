00f7062f02c1a003eed04a2f3d20c9de7e77f17e
Dark_zarich <darkzarichv2@gmail.com>
2019-01-28 18:29:23 +0300

    initial commit

--- tests/unit/counter.spec.js
+++ tests/unit/counter.spec.js
@@ -12,4 +12,11 @@ describe("Counter.vue", () => {
   it("has a button ", () => {
     expect(wrapper.contains("button")).to.be.true;
   });
+
+  it("button click should increment the count", () => {
+    expect(wrapper.vm.count).to.equal(0);
+    const button = wrapper.find("button");
+    button.trigger("click");
+    expect(wrapper.vm.count).to.equal(1);
+  });
 });
